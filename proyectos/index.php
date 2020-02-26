80ee060319991e8ce57baffc19729231c92bf2ac
Alberto <alberto.noriega@vlim.com.mx>
2020-02-26 17:34:12 -0600

    Update: Tabla de proyectos, se implemento el modal en agregar nuevo proyecto

--- proyectos/index.php
+++ proyectos/index.php
@@ -24,8 +24,100 @@ include("../head.php")
             <div class="white-box">
                 <div class="d-flex justify-content-between">
                 <h3 class="box-title m-b-0">Proyectos</h3>
-                <a href="../agregarProyecto"><button class="fcbtn btn btn-success btn-outline btn-1b">Agregar</button></a>
+                <button class="fcbtn btn btn-success btn-outline btn-1b" data-toggle="modal" data-target="#myModal">Agregar</button>
                 </div>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="myModal" role="dialog">
+                        <div class="modal-dialog">
+
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                <div class="modal-header" style="padding:35px 50px;">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <font4>Nuevo Proyecto</font4>
+                                </div>
+                                <div class="modal-body" style="padding:40px 50px;">
+                                    <form class="user" id="form">
+
+                                        <div class="form-group row">
+                                            <div class="col-sm-12 mb-3 mb-sm-0">
+                                                Tipo de Proyecto
+                                                <input type="text" class="form-control" id="estatus_name" placeholder="Ingresa el tipo de Proyeycto ..." required="" data-error="Por favor, ingresa el tipo de Proyecto.">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-sm-12 mb-3 mb-sm-0">
+                                                Nombre del Proyecto
+                                                <input type="text" class="form-control" id="estatus_name" placeholder="Ingresa el Nombre del Proyeycto ..." required="" data-error="Por favor, ingresa el Nombre del Proyecto.">
+                                            </div>
+                                            <div class="col-sm-12 mb-3 mb-sm-0">
+                                                Fecha de Entrega
+                                                <input type="date" class="form-control" id="estatus_name" placeholder="" required="" data-error="Por favor, ingresa la fecha de entrega.">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-sm-6">
+                                                Estatus
+                                                <select class="form-control" data-placeholder="Choose a Category" tabindex="1">
+                            <?php
+								include "../conn.php";
+								$queryBuscaPerfil = "SELECT * FROM `cat_estatus` ORDER BY estatus ASC";
+								$resultPerfil = mysqli_query($conn, $queryBuscaPerfil);
+								if ($resultPerfil) {
+									$num_rows = mysqli_num_rows($resultPerfil);
+									if ($num_rows > 0) {
+										while ($row = mysqli_fetch_array($resultPerfil)) {
+											echo '<option value="' . $row['id_estatus'] . '"> ' . $row['estatus'] . ' </option>';
+										}
+									} else {
+										echo "No hay registros";
+									}
+								} else {
+									echo "Error: " . mysqli_error($conn);
+								}
+								mysqli_free_result($resultPerfil);
+								mysqli_close($conn);
+								?>
+                        </select>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                Colaborador
+                                                <select class="form-control" data-placeholder="Choose a Category" tabindex="1">
+                        <?php
+								include "../conn.php";
+								$queryBuscaPerfil = "SELECT * FROM `colaboradores`";
+								$resultPerfil = mysqli_query($conn, $queryBuscaPerfil);
+								if ($resultPerfil) {
+									$num_rows = mysqli_num_rows($resultPerfil);
+									if ($num_rows > 0) {
+										while ($row = mysqli_fetch_array($resultPerfil)) {
+											echo '<option value="' . $row['id_colaborador'] . '"> ' . $row['nombre'] . ' </option>';
+										}
+									} else {
+										echo "No hay registros";
+									}
+								} else {
+									echo "Error: " . mysqli_error($conn);
+								}
+								mysqli_free_result($resultPerfil);
+								mysqli_close($conn);
+								?>
+                        </select>
+                                            </div>
+                                        </div>
+                                        <div align="center">
+                                            <button class="fcbtn btn btn-success btn-outline btn-1b" id="guardar">
+                                                GUARDAR
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 <br>
                 <div class="table-responsive">
                     <table class="table table-bordered">
